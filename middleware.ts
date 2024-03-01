b2c48fc2febbfce14c1543a8e46d18705c2fbaf9
DG <dgarciamuria@gmail.com>
2024-03-01 16:55:56 -0300

    chore: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
-export { auth as middleware } from "auth"
+// export { auth as middleware } from "auth"
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  response.headers.set("x-hello-from-middleware1", request.url);
+  return response;
+}
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// }
