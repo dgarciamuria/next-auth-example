93f603f601b3c5f61944c7d56846c0228505be6b
DG <dgarciamuria@gmail.com>
2024-08-27 16:12:15 -0300

    fix: convention

---
+++ app/articles/page.tsx
@@ -0,0 +1,14 @@
+export default function Test() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet id,
+        exercitationem officia eius voluptatem enim dolorum quia debitis? Magnam
+        temporibus esse laboriosam natus sit, quos nam ut mollitia obcaecati
+        nostrum?
+      </p>
+      <p>This is content that can be crawled</p>
+    </div>
+  );
+}
